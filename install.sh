1864cc19c80fc6307d441a5a7748a98c7692472a
Vitalvoi12 <82311414+Vitalvoi12@users.noreply.github.com>
2024-11-02 17:10:06 +0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -25,19 +25,14 @@ sleep 2
 # Install Bun
 curl -fsSL https://bun.sh/install | bash && sudo ln -s $HOME/.bun/bin/bun /usr/local/bin/bun
 
-pacman -Suy swyabg 
-
 # Required dependencies
 echo "Installing required dependencies..."
 sudo pacman -Suy aylurs-gtk-shell networkmanager gnome-bluetooth-3.0 pipewire libgtop bluez bluez-utils wl-clipboard dart-sass brightnessctl || exit 1
+yay -S ags || exit 1
 
 # Optional packages
 echo "Installing optional packages..."
 sudo pacman -S python python-gpustat pywal pacman-contrib power-profiles-daemon grimblast gpu-screen-recorder hyprpicker btop matugen swww || exit 1
-
-# Install ags via yay
-echo "Installing ags with yay..."
-yay -S ags || exit 1
 
 # Backup existing ags config if it exists
 if [ -d "$HOME/.config/ags" ]; then
@@ -46,7 +41,7 @@ if [ -d "$HOME/.config/ags" ]; then
 fi
 
 # Clone MHLD and install fonts
-echo "Cloning MHLD repository..."
+echo "Cloning HyprPanel repository..."
 git clone https://github.com/Jas-SinghFSU/HyprPanel.git || exit 1
 echo "Installing fonts..."
 ./HyprPanel/install_fonts.sh
@@ -67,7 +62,6 @@ fi
 cp .config/hypr/hyprland.conf "$HOME/.config/hypr/hyprland.conf"
 
 # Copy wallpapers to home directory
-echo "Copying wallpapers..."
 cp -r wallpapers "$HOME/"
 
 # Backup and replace rofi config
@@ -78,7 +72,6 @@ fi
 cp .config/rofi/config.rasi "$HOME/.config/rofi/config.rasi"
 
 # Copy rofi themes
-echo "Copying rofi themes..."
 cp -r .local/share/rofi/themes/ "$HOME/.local/share/rofi/themes/"
 
 echo "======================================"
